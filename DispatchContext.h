04de0b191f8f1964b9ae20c9a2d37ddbd77e2c7c
Robert Olson <olson@mcs.anl.gov>
2020-06-16 16:26:39 -0500

    add AuthToken for hte user associated with this session.

--- DispatchContext.h
+++ DispatchContext.h
@@ -3,17 +3,21 @@
 
 #include <boost/asio/spawn.hpp>
 #include <boost/asio/deadline_timer.hpp>
+#include "AuthToken.h"
 
 class DispatchContext
 {
 public:
-    DispatchContext(boost::asio::yield_context &y,  boost::asio::executor e)
+    DispatchContext(boost::asio::yield_context &y,  boost::asio::executor e, const AuthToken &t)
 	: yield(y)
 	, timer(e)
+	, token(t)
     {}
 
+    
     boost::asio::yield_context yield;
     boost::asio::deadline_timer timer;
+    AuthToken token;
 };
 
 
